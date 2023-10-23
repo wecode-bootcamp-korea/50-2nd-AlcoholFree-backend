feadffae0a3ca20c75ffbdb2f86dd8ce3df738c4
LEE <dlgoals3384@gmail.com>
2023-10-23 17:55:14 +0900

    초기세팅

---
+++ src/routes/productRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const userRouter = require("../controlles/productController");
+
+
+//게사글 등록
+
+
+
+module.exports ={
+    router
+}
