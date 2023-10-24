ff8fe1adbd8ba82735c6c58d735e846a6d9838ea
LEE <dlgoals3384@gmail.com>
2023-10-24 17:08:24 +0900

    login API 수정

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require("../controlles/userController");
 const verfiyToken = require("../middlewares/verfiyToken")
 
-
 //로그인
 router.post("/login", userController.login);
 
