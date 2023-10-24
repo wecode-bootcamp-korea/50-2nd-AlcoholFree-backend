a1b5193f7a0640b36e42a169972282c7ed8dbf3a
LEE <dlgoals3384@gmail.com>
2023-10-24 17:34:14 +0900

    회원가입 수정

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,8 @@
 const { SimpleConsoleLogger } = require("typeorm");
 const userDao = require("../models/userDao");
 const { json } = require("body-parser");
+const verfiyToken = require("../middlewares/verfiyToken")
+const bcrypt = require("../middlewares/bcrypt")
 
 
 // 회원가입
@@ -14,7 +16,6 @@ const signup = async(email, password, name, phoneNumber, birthDay, address) => {
     if(dbUserData.length !== 0){
         return false;
     }
-  
     //email 정규식
     const emailValidation = new RegExp("^^[a-zA-Z0-9+-_.]+@{1}[a-zA-Z0-9-]+\.{1}[a-zA-Z0-9-.]+$");
     
@@ -32,23 +33,30 @@ const signup = async(email, password, name, phoneNumber, birthDay, address) => {
         "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{10,20})"
     );
 
+    console.log(password);
+
     if(!pwValidation.test(password)){
         const err = new Error();
         err.message = "패스워드는 대,소문자 및 숫자,특수문자 1자 이상을 이용하여 10~20자리를 만들어 주세요."
         err.statusCode = 409;
         throw err;
+    }else{
+        console.log("패스워드 형식이 올바릅니다.")
     }
+
+    // password 암호화
+     const saltRound = 10;
+     const hashedPassword = await bcrypt.makehash(password, saltRound);
+     password = hashedPassword; // 암호화된 데이터를 password에 넣기
 
     }catch(err){
         const error = new Error();
-        error.message = "패스워드는 대,소문자 및 숫자,특수문자 1자 이상을 이용하여 10~20자리를 만들어 주세요."
+        error.message = "입력 데이터를 확인해 주세요."
         error.statusCode = 409;
         throw error;
     }
-
     //데이터 저장
     try{
-        
         const result = await userDao.signup(email, password, name, phoneNumber, birthDay, address);
         return result
 
@@ -59,6 +67,7 @@ const signup = async(email, password, name, phoneNumber, birthDay, address) => {
         throw error;
     }
 }
+
 
 
 module.exports = {
