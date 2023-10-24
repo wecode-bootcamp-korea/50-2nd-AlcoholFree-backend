a6b36adfd89c5fa952bc4a2f1c0f9c2b327185eb
LEE <dlgoals3384@gmail.com>
2023-10-24 16:56:30 +0900

    로그인 API

--- src/controlles/userController.js
+++ src/controlles/userController.js
@@ -1,29 +1,27 @@
 const userService = require("../services/userService");
 
 
-// 회원가입
-const signup = async(req, res) => {
-    
-    const {email, password, name, phoneNumber, birthDay, address} = req.body
+//로그인
+const login = async(req, res) => {
 
-    try { 
-        // 요청 정보 확인
-        if(!email || !password || !name || !phoneNumber || !birthDay || !address){
-            return res.json({message : "Key_error"});
-         }
-        const result = await userService.signup(email, password, name, phoneNumber, birthDay, address);
+    const {email, password} = req.body;
+    try{
+        const result = await userService.login(email, password);
+        const token = ""
 
         if(result === false){
-            return res.json({message : "email이 중복 됩니다."});
+            return res.json({message : "USER_NOT_FOUND"})
+        }else{
+            const token = result;
+            return res.json({accsessToken : result, message : "LOGIN_SUCCESS"});
         }
-        return res.json({message : "created_success", result});
-        
+        return res.json({accsessToken : result});
     }catch(err){
-        return res.json({massage : err})
+        return res.json({message : err})
     }
+
 }
 
-
 module.exports = {
-    signup
+    login
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controlles/userController");
+const verfiyToken = require("../middlewares/verfiyToken")
 
 
-//회원가입
-router.post("/signup", userController.signup)
+//로그인
+router.post("/login", userController.login);
 
 
 module.exports = {
