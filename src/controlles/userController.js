2db8efb3afd04fed49f99ff478536f41c3ab487f
LEE <dlgoals3384@gmail.com>
2023-10-26 07:32:28 +0900

    결제 API 개발 준비

--- src/controlles/userController.js
+++ src/controlles/userController.js
@@ -2,27 +2,8 @@ const userService = require("../services/userService");
 
 
 
-//로그인
-const login = async(req, res) => {
 
-    const {email, password} = req.body;
-    try{
-        const result = await userService.login(email, password);
-        const token = ""
-
-        if(result === false){
-            return res.json({message : "USER_NOT_FOUND"})
-        }else{
-            const token = result;
-            return res.json({accsessToken : result, message : "LOGIN_SUCCESS"});
-        }
-        return res.json({accsessToken : result});
-    }catch(err){
-        return res.json({message : err})
-    }
-
-}
 
 module.exports = {
-     login
+     
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const userController = require("../controlles/userController");
 const verfiyToken = require("../middlewares/verfiyToken")
 
-//로그인
-router.post("/login", userController.login);
+
 
 
 module.exports = {
