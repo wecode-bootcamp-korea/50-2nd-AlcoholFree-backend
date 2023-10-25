2e73bd8b4ed924061c5f280cb59957721ac9a389
LEE <dlgoals3384@gmail.com>
2023-10-26 08:50:57 +0900

    userService 수정

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,5 @@
 const userDao = require("../models/userDao");
 const { SimpleConsoleLogger } = require("typeorm");
-const userDao = require("../models/userDao");
 const { json } = require("body-parser");
 const bcrypt = require("../middlewares/bcrypt");
 const verfiyToken = require("../middlewares/verfiyToken");
