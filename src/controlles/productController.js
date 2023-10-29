fcbdebd94fb6967bb8399f2b7eca7008f17f2c73
leehaemin <dlgoals3384@gmail.com>
2023-10-29 11:38:05 +0900

    작성중

--- src/controlles/productController.js
+++ src/controlles/productController.js
@@ -2,7 +2,20 @@ const productService = require("../services/productService");
 
 
 
+// 장바구니에 담긴 상품을 정하고 결제 버튼을 눌렀을때
 const cost = async(req, res) => {
+
+    const {totalPrice} = req.body;
+
+    try{
+        const result = await productService.cost(totalPrice);
+        return result;
+    }catch(err){
+        console.log(err);
+        const error = new Error();
+        error.message = "컨트롤러 에러";
+        throw error;
+    }
 
 }
 
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,8 +1,18 @@
 const productDao = require("../models/productDao")
 
 
-const cost = async() => {
+// 장바구니에 담긴 상품을 정하고 결제 버튼을 눌렀을때
+const cost = async(cartId, name, avm, orgin, price, count, totalPrice) => {
 
+    try{
+        const result = await productDao.cost(cartId, name, avm, orgin, price, count, totalPrice);
+        return result;
+    }catch(err){
+        console.log(err);
+        const error = new Error();
+        error.message = "서비스 에러";
+        throw error;
+    }
 }
 
 
