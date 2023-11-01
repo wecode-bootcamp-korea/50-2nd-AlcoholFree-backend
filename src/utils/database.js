109758d33d3c09180c939a1f1f83e55b5747d2af
LEE <dlgoals3384@gmail.com>
2023-11-01 15:48:45 +0900

    database 파일 분리

---
+++ src/utils/database.js
@@ -0,0 +1,29 @@
+const { DataSource, IsNull } = require('typeorm');
+const path = require("path")
+const envFilePaht = path.resolve(__dirname, "../utils", ".env");
+const dotenv = require("dotenv");
+const { error } = require('console');
+dotenv.config({path :envFilePaht });
+
+const appDataSource = new DataSource({
+	type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+})
+
+appDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error occurred during Data Source initialization", err);
+  });
+
+
+
+  module.exports = {
+    appDataSource
+  }
