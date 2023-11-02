3f56a0a293400e768b7a3c19d4455ec84e6974f2
Kimmunyeong <mykim9352@gmail.com>
2023-11-02 12:51:17 +0900

    main api--

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controlles/productController");
+const productController = require("../controllers/productController");
 const authorization = require("../middlewares/verfiyToken");
 
 router.get('/main', authorization.verfiyToken, productController.selectProduct);
